e29312171ac5e3b826ae03e7e9023e764072548d
arfurs <arfurs@163.com>
2018-11-04 01:20:53 +0800

    后台获取单个成员详情接口

--- app/Http/Controllers/Api/Admin/CompanyMemberController.php
+++ app/Http/Controllers/Api/Admin/CompanyMemberController.php
@@ -11,6 +11,14 @@ use App\Http\Response\Transformers\Admin\CompanyMemberTransformer;
 class CompanyMemberController extends Controller
 {
     /**
+     * 展示单个公司成员详情
+     */
+    public function show(CompanyMember $companyMember)
+    {
+        return $this->response->item($companyMember, new CompanyMemberTransformer());
+    }
+
+    /**
      * 创建公司成员
      */
     public function create(CreateCompanyMemberRequest $request, CompanyMember $companyMember)
