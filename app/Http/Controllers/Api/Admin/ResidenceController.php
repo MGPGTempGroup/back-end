50a34c31db9a4032b24cf4a133d16af784d601cd
arfurs <arfurs@163.com>
2018-11-07 18:28:23 +0800

    后台修改出售房屋数据

--- app/Http/Controllers/Api/Admin/ResidenceController.php
+++ app/Http/Controllers/Api/Admin/ResidenceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Requests\Admin\CreateResidenceRequest;
+use App\Http\Requests\Admin\UpdateResidenceRequest;
 use App\Http\Response\Transformers\Admin\ResidenceTransformer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -16,6 +17,16 @@ class ResidenceController extends Controller
      * 创建出售房屋数据
      */
     public function store(CreateResidenceRequest $request, Residence $residence)
+    {
+        $residence->fill($request->all());
+        $residence->save();
+        return $this->response->item($residence, new ResidenceTransformer());
+    }
+
+    /**
+     * 修改出售房屋数据
+     */
+    public function update(UpdateResidenceRequest $request, Residence $residence)
     {
         $residence->fill($request->all());
         $residence->save();
