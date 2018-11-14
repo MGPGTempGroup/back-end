fdda9d5c848061807df6ad16653dcdeabc2ba4cd
arfurs <arfurs@163.com>
2018-11-14 17:42:54 +0800

    设置Schema构建数据库String类型时最大字符长度为191（解决utf8mb4编码下最大存储191个字符才能建立索引的问题...）

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 设定默认字符长度限制，解决utf8mb4最大字符问题
+        Schema::defaultStringLength(191);
     }
 
     /**
