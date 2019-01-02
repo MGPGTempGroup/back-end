b4da97048ed376c784830190d5107fa3720246f1
arfurs <arfurs@163.com>
2019-01-02 21:27:57 +0800

    建立备注表（多态）

---
+++ database/migrations/2019_01_02_082005_create_remarks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRemarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('remarks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->mediumText('content'); // 内容
+            $table->unsignedInteger('creator_id'); // 创建人（管理员）
+            // 多态关联字段
+            $table->string('come_from_type');
+            $table->unsignedInteger('come_from_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('remarks');
+    }
+}
